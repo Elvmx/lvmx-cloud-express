e6b8301d59649eec2e5618d239a0cd0a4e4db1f8
Elvmx47 <elvmx47@outlook.com>
2023-12-29 15:09:49 +0800

    update

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
-const { Sequelize, DataTypes } = require("sequelize");
+const {Sequelize, DataTypes} = require("sequelize");
 
 // 从环境变量中读取数据库配置
-const { MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_ADDRESS = "" } = process.env;
+const {MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_ADDRESS = ""} = process.env;
 
 const [host, port] = MYSQL_ADDRESS.split(":");
+
+console.log('MYSQL_ADDRESS', MYSQL_ADDRESS);
 
 const sequelize = new Sequelize("nodejs_demo", MYSQL_USERNAME, MYSQL_PASSWORD, {
   host,
@@ -22,7 +24,7 @@ const Counter = sequelize.define("Counter", {
 
 // 数据库初始化方法
 async function init() {
-  await Counter.sync({ alter: true });
+  await Counter.sync({alter: true});
 }
 
 // 导出初始化方法和模型
